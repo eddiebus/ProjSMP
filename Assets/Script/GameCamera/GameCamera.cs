6749b755da8a6264b83cc8d7949bc3aacd31a2f3
Edwin <71640744+eddiebus@users.noreply.github.com>
2023-06-07 16:18:40 +0100

    Camera Stuff

--- Assets/Script/GameCamera/GameCamera.cs
+++ Assets/Script/GameCamera/GameCamera.cs
@@ -1,13 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameCamera : MonoBehaviour
 {
     private Camera _myCamera;
-    private float CameraHeight = 720.0f;
+    private float CameraHeight = 1280.0f;
     private void _UpdateProperties()
     {
+        float width = Screen.width;
+        if (width < 576)
+        {
+            width = 576;
+        }
+        _myCamera.projectionMatrix = Matrix4x4.Ortho(
+            (float)-width / 2.0f,
+            (float)width / 2.0f,
+            -CameraHeight / 2,
+            CameraHeight / 2,
+            0.3f,1000
+            );
+
+        float TargetHeightRatio = (float)20 / 9;
+        float CurrentHeightRatio = (float)Screen.height / Screen.width;
+        Debug.Log($"Target = {TargetHeightRatio} | Current = {CurrentHeightRatio}");
+
+
 
     } 
     // Start is called before the first frame update
@@ -25,5 +44,6 @@ public class GameCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        _UpdateProperties();
     }
 }
